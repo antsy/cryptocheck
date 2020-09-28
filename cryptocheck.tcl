f9389e20e5ce2bbc600a1acbc5e6067f4c540c64
Antti Humalamäki <antti.humalamaki@sc5.io>
2020-09-29 00:17:25 +0300

    Initial commit

---
+++ cryptocheck.tcl
@@ -0,0 +1,174 @@
+# antsy's cryptocurrency price check script
+
+
+package require Tcl
+package require http
+package require json
+package require tls
+::http::register https 443 [list ::tls::socket -autoservername true]
+
+namespace eval cryptocheck {
+
+# Eggdrop bindings
+bind pub - !crypto [namespace current]::cryptocheckpub
+bind msg f !crypto [namespace current]::cryptocheckmsg
+
+# Global variables
+set ccVersion "Cryptocurrency price check script 1.0"
+set outputCurrency "eur"
+set outputSymbol "€"
+
+putlog "Loaded script $ccVersion"
+
+##
+# Handler for calling the script via public message
+#
+# @param nick    User nickname
+# @param host    User hostname
+# @param handle  User in bot
+# @param channel Channel
+# @param text    Parameters
+proc cryptocheckpub {nick host handle channel text} {
+  putlog "Cryptocurrency check: $nick ($handle/$host) requested \[$text\] at $channel"
+  if {$text == ""} {
+    variable ccVersion
+    msg $channel "$ccVersion - Usage: !crypto <bitcoin/ethereum/ripple/etc...> (you can query multiple, separated by space)"
+    return
+  }
+  # Clean input
+  set text [string trim $text]
+
+  set currency [split $text " "]
+  set output [fetch $currency]
+  putlog $output
+  set output [prettyPrint $output]
+  putlog $output
+
+  set length [string length $output]
+  if {$length > 1} {
+    msg $channel $output
+  }
+}
+
+##
+# Handler for calling the script via public message
+#
+# @param nick    User nickname
+# @param uhost   User hostname
+# @param handle  User in bot
+# @param text    Parameters
+proc cryptocheckmsg {nick uhost handle text} {
+  putlog "Cryptocurrency check: $nick ($handle!$uhost) requested \[$text\] as privmsg"
+  if {$text == ""} {
+    variable ccVersion
+    msg $nick "$ccVersion - Usage: !crypto <bitcoin/ethereum/ripple/etc...> (you can query multiple, separated by space)"
+    return
+  }
+  # Clean input
+  set text [string trim $text]
+
+  set currency [split $text " "]
+  set output [fetch $currency]
+  set output [prettyPrint $output]
+
+  set length [string length $output]
+  if {$length > 1} {
+    msg $nick $output
+  }
+}
+
+##
+# Send response to user.
+# Separates output to multiple lines if the message is too long (for IRCnet)
+#
+# @param dest   Target, can be nick or channel.
+# @param data   The message.
+proc msg {dest data} {
+   set len [expr {512-[string len ":$::botname PRIVMSG $dest :\r\n"]}]
+   foreach line [wordwrap $data $len] {
+      puthelp "PRIVMSG $dest :$line"
+   }
+}
+
+##
+# Format the result data
+#
+# @param coinData   Result data Dictionary from Coingecko
+# @return           Message formatted for IRC
+proc prettyPrint { data } {
+  set output [list]
+  variable outputCurrency
+  variable outputSymbol
+  dict for {coinName coinData} $data {
+    dict with coinData {
+      set coinValue [subst $$outputCurrency]
+      set coinValue [format {%0.3f} $coinValue]
+      set valueChange [format {%+0.2f} $eur_24h_change]
+      if {$eur_24h_change > 0} {
+        # Value gone up, color it green
+        set valueChange [colorize $valueChange 3]
+      } else {
+        # Value gone down, color it red
+        set valueChange [colorize $valueChange 4]
+      }
+
+      set str [join [list $coinName ": " [bold $coinValue] $outputSymbol " (" $valueChange "%)"] ""]
+      lappend output $str
+    }
+  }
+
+  return [join $output " "]
+}
+
+##
+# Colorize string and reset back to default color
+#
+# @param str    String to be colorized
+# @param color  VGA color
+proc colorize {str color} {
+  set output "\003$color"
+  append output $str
+  append output "\003"
+  return $output
+}
+
+##
+# Bold string
+#
+# @param  str   Input string
+# @return       Formatted string
+proc bold {str} {
+  return "\002$str\002"
+}
+
+##
+# Fetches the prices from the Coingecko's free API.
+#
+# @see https://www.coingecko.com/api/documentations/v3
+#
+# @param currency    Currency or currencies to check
+# @return {dict}     Result from request
+proc fetch { currency } {
+  variable ccVersion
+  variable outputCurrency
+  set ids [join $currency "%2C"]
+  set url "https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${outputCurrency}&include_last_updated_at=true&include_24hr_change=true"
+
+  # debug fetch url
+  #putlog "fetch url: $url"
+
+  set token [http::config -useragent "$ccVersion"]
+  set token [http::geturl $url]
+
+  set result [json::json2dict [http::data $token]]
+
+  # debug result
+  #putlog "result: $result"
+
+  # TODO: check if $last_updated_at value has gone old, output some warning to user
+
+  http::cleanup $token
+  return $result
+}
+
+} ;#end namespace
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Cryptocurrency price check script for Eggdrop IRC bots
+
+What the title says, Eggdrop IRC script which will fetch prices of cryptocurrencies and display the current value and last 24h change.
+
+## Usage
+
+Message the bot `!crypto` command to get current price of the coin in Euros. Command works both as private message or as public message in any channel where your bot is listening. The value will be read from the Coingecko's API. Please refer to the [Coingecko API documentation](https://www.coingecko.com/api/documentations/v3) to get the actual [names of the coins](https://www.coingecko.com/api/documentations/v3#/coins/get_coins_list). If you prefer dollars💵 over euros💶 just edit the `outputCurrency` and `outputSymbol` values of the script.
+
+
+## Installation
+
+Save the script somewhere where your Eggdrop can read it. Add the script to your bots configuration file with `source <path>` command. Run `.rehash` in party line, but be aware that if the script doesn't manage to load properly (Includes missing?) your bot will crash! Or alternatively you can simply restart your bot.
+
+
+### Possible problems
+
+If you already have [Eggdrop](https://www.eggheads.org/install) (The IRC bot), [Tcl/Tk](https://www.tcl.tk/software/tcltk/) (Required to run scripts with the bot), [Tcllib](https://www.tcl.tk/software/tcllib/) (Required to handle JSON) and [Tls](https://wiki.tcl-lang.org/page/tls) (Required to access HTTPS) you should be fine using the script as it is.
+
+However if you are running TCL in an environment where you don't have root access you might have problems including the Tcllib in your `auto_path` path definition. In that case you need to add the Tcllib via include in the script itself. Adding a line `lappend auto_path /path/to/your/copy/of/tcllib/lib` at the beginning of your script file should do the trick. If there is a better way of adding Tcllib to Tcl path, please let me know.
+
+## Thanks
+
+If you like this, please consider tipping:
+
+ * BTC: `3P7mfD1dCebi1LPEDByk5Q68bPDPzgLUD1`
+ * ETH: `0xE511275174FA446735AECE520717aaD5932e47e2`
+ * XRP: `rw2ciyaNshpHe7bCHo4bRWq6pqqynnWKQg:2205678536`
